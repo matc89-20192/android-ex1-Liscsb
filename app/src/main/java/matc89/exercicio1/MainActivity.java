1c7a1bf3c2bba07a0882db9ece8dc4d1b117d6e3
Lisandra Calmon São Bernardo <lisandra.c.bernardo@ericssoninovacao.com.br>
2019-09-07 20:13:41 -0300

    resolucao do exercicio 1

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -1,13 +1,60 @@
 package matc89.exercicio1;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.*;
+import android.widget.*;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editNome;
+    Button btnCumprimentar;
+    TextView labelMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editNome = (EditText) findViewById(R.id.editNome);
+        labelMensagem = (TextView) findViewById(R.id.labelMensagem);
+        btnCumprimentar = (Button) findViewById(R.id.btnCumprimentar);
+
+        if (savedInstanceState != null) {
+            String label = savedInstanceState.getString("label");
+            labelMensagem.setText(label);
+        }
+
+        btnCumprimentar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String nome = editNome.getText().toString();
+                String label = "Alô, ";
+                label+= nome;
+                label+= "!";
+                labelMensagem.setText(label);
+            }
+        });
+
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("label", labelMensagem.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String label = savedInstanceState.getString("label");
+
+    }
+
+
+
 }
